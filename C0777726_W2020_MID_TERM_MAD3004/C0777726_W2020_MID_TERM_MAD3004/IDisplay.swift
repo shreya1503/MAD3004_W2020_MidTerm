234143ff0d38ce9e92bced5637fd661620123fbf
Shreya <shreyavaghasia99@gmail.com>
2020-02-13 18:18:25 -0500

    Creating IDisplay Protocol

---
+++ C0777726_W2020_MID_TERM_MAD3004/C0777726_W2020_MID_TERM_MAD3004/IDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IDisplay.swift
+//  C0777726_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay
+{
+    func display()
+}
