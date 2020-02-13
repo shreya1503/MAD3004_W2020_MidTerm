3b311c672140f67a17d97a4f0bb098e63b0d194a
Shreya <shreyavaghasia99@gmail.com>
2020-02-13 18:01:37 -0500

    Creating Customer Class

---
+++ C0777726_W2020_MID_TERM_MAD3004/C0777726_W2020_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//  C0777726_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer
+{
+    
+}
