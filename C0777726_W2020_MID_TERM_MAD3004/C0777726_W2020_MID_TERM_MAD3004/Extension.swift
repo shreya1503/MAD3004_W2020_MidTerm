1f91ee304c19c2f83819e561f879c1ee45b4a4dc
Shreya <shreyavaghasia99@gmail.com>
2020-02-18 16:38:29 -0500

    [SV] Updating And Changes Made In Extension

--- C0777726_W2020_MID_TERM_MAD3004/C0777726_W2020_MID_TERM_MAD3004/Extension.swift
+++ C0777726_W2020_MID_TERM_MAD3004/C0777726_W2020_MID_TERM_MAD3004/Extension.swift
@@ -14,14 +14,14 @@ extension String
     func Emailvalidation() -> Bool
     {
         let EmailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let EmailTest = NSPredicate(format : "Self match %@" , EmailRegex)
+        let EmailTest = NSPredicate(format : "SELF MATCHES %@" , EmailRegex)
         return EmailTest.evaluate(with: self)
     }
     
     func mobilevalidation() -> Bool
     {
         let MobileNumberRegex = "[0-9]{10}"
-        let MobileTest =  NSPredicate(format:"Self match %@",MobileNumberRegex)
+        let MobileTest =  NSPredicate(format:"SELF MATCHES %@",MobileNumberRegex)
         return MobileTest.evaluate(with:self)
     }
 
